e6fafb0ad392f3f54cd6f38c5df80c7d040c40a9
ryanm74 <G00340535@gmit.ie>
2020-05-13 16:51:06 +0100

    First Commit

--- src/main/java/ie/gmit/test/Run.java
+++ src/main/java/ie/gmit/test/Run.java
@@ -1,87 +1,97 @@
 package ie.gmit.test;
 
-import java.util.ArrayList;
 
+import java.util.ArrayList;
+import java.lang.String;
 import ie.gmit.Programme;
 import ie.gmit.Student;
 import ie.gmit.Module;
 import ie.gmit.Grades;
+import ie.gmit.Attendance;
 import org.joda.time.DateTime;
 
-public class Run {
+public class Run<Students> {
 
-    public static void main(String[] args){}
+    public static void main(String[] args) {
 
-    //Creating Students and adding them to the list of students
-    ArrayList<Student> Students = new ArrayList<~>();
-    Student Peter = new Student("Peter Treacy", "13/05/1995", 25);
-    Student Nicola = new Student("Nicola Costello","12/4/1996",24);
-    Student Megan = new Student("Megan Fitzgerald", "17/7/1994", 25);
-    Student Dean = new Student("Dean Glavey", "03/03/1996", 24);
-    Student John = new Student("John Lawless", "05/06/1990", 29);
-    Student Tim = new Student("Tim Mahoney", "20/11/1995", 24);
-
-    Students.add(Peter);
-    Students.add(Nicola);
-    Students.add(Megan);
-    Students.add(Dean);
-    Students.add(John);
-    Students.add(Tim);
-
-    //Creating list of modules
-    Module CloudComputing = new Module("Cloud Computing","CC101");
-    Module Maths = new Module("Mathematics", "MA203");
-    Module BioMedScience = new Module("Biomedical Science", "BS511");
-    Module BusinessInfo = new Module("Business Information Systems", "B1222");
-    Module SoftwareEng = new Module("Software Engineering with Test", "SE333");
-    Module MobileAppDev = new Module("Mobile App Development", "MAD987");
-
-    //Adding students to modules
-    CloudComputing.addStudent(Peter);
-    CloudComputing.addStudent(Dean);
-
-    Maths.add(Dean);
-    Maths.add(Tim);
-
-    BioMedScience.add(Megan);
-
-    BusinessInfo.add(John);
-
-    SoftwareEng.add(Nicola);
-
-    MobileAppDev.add(Peter);
-
-    //Creating new courses
-    Programme IT = new Programme("Software Development & Electronic Engineering",new DateTime());
+        //Creating Students and adding them to the list of students
+        ArrayList<Student> Students = new ArrayList<Student>();
+        Student Peter = new Student("Peter Treacy", "13/05/1995", 25);
+        Student Nicola = new Student("Nicola Costello", "12/4/1996", 24);
+        Student Megan = new Student("Megan Fitzgerald", "17/7/1994", 25);
+        Student Dean = new Student("Dean Glavey", "03/03/1996", 24);
+        Student John = new Student("John Lawless", "05/06/1990", 29);
+        Student Tim = new Student("Tim Mahoney", "20/11/1995", 24);
 
 
-    //adding modules to the course programme
-    IT.addModule(CloudComputing);
-    IT.addModule(Maths);
-    IT.addModule(SoftwareEng);
-    IT.addModule(MobileAppDev);
+        Students.add(Peter);
+        Students.add(Nicola);
+        Students.add(Megan);
+        Students.add(Dean);
+        Students.add(John);
+        Students.add(Tim);
 
-    Peter.addGrade(CloudComputing) = "A";
+
+        //Creating list of modules
+        ArrayList<Module> Modules = new ArrayList<Module>();
+        Module CloudComputing = new Module("Cloud Computing", "CC101");
+        Module Maths = new Module("Mathematics", "MA203");
+        Module BioMedScience = new Module("Biomedical Science", "BS511");
+        Module BusinessInfo = new Module("Business Information Systems", "B1222");
+        Module SoftwareEng = new Module("Software Engineering with Test", "SE333");
+        Module MobileAppDev = new Module("Mobile App Development", "MAD987");
+
+        //Adding students to modules
+        CloudComputing.addStudent(Peter);
+        CloudComputing.addStudent(Dean);
 
 
-    Peter.setProgramme(IT);
+        Maths.addStudent(Peter);
+        Maths.addStudent(Dean);
+        Maths.addStudent(Tim);
 
-    System.out.println("List of students and their results");
 
-    for(Student s : Students){
-        System.out.println(s.toString());
-        System.out.println(s.getProgramme().toString());
+        //Creating new courses
+        Programme SEE = new Programme("Software & Electronic Engineering", new DateTime(2019, 9, 5, 9, 0), new DateTime(2020, 5, 19, 17, 0));
 
-        System.out.println("List of results");
-        for(Module mod:s.getProgramme().getModules())
-        {
-            if(mod.getStudents().contains(s)){
-                System.out.println(mod.toString());
 
+        //adding modules to the course programme
+
+        SEE.addModule(Maths);
+        SEE.addModule(CloudComputing);
+        SEE.addModule(Maths);
+        SEE.addModule(SoftwareEng);
+        SEE.addModule(MobileAppDev);
+
+
+        //Grades  = new Grades("")
+        //CloudComputing.addGrade(Peter) = "A";
+
+
+        Peter.setProgramme(SEE);
+        Nicola.setProgramme(SEE);
+        Megan.setProgramme(SEE);
+        Dean.setProgramme(SEE);
+        John.setProgramme(SEE);
+        Tim.setProgramme(SEE);
+
+
+        System.out.println("List of students and their results");
+//
+
+        for (Student student : Students) {
+            System.out.println(student.toString());
+            System.out.println(student.getProgramme().toString());
+
+            System.out.println("---List of Modules----");
+            for(Module module:student.getProgramme().getModules())
+            {
+                if(module.getStudents().contains(student)){
+                    System.out.println(module.toString());
+                }
             }
+            System.out.println("\n");
         }
-        System.out.println("\n");
     }
-
 
 }
